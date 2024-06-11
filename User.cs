08399e5f6d5c5268c23cf845cbc71556db6c302d
unknown <alexannder2201@hotmail.com>
2024-06-10 19:08:15 -0500

    commit to reverse

--- User.cs
+++ User.cs
@@ -2,8 +2,7 @@ public class User
 {
      public int id { get; set; }
     public string Firstname { get; set; }
-    public string Lastname { get; set; }
-     public string Email { get; set}
-      public string Password { get; set}
+    
+    public string Password { get; set}
 }
     
