b580fdf1efe06974a0f065c75629f3d64ed60cf7
unknown <alexannder2201@hotmail.com>
2024-06-10 19:05:58 -0500

    Complete classes

---
+++ Employee.cs
@@ -0,0 +1,7 @@
+public class Employee
+{
+    public int id { get; set; }
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    public decimal Salary { get; set; }   
+}
